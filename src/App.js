36feeb2bf2eb930794ba9687cdc47a187abc9132
Valentin <valentin.cluzan33@gmail.com>
2019-08-21 14:20:02 +0200

    add state on app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Quote from "./Quote";
+import Quotes from "./Quotes";
+import Lamp from "./Lamp";
+
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            working: false
+        };
+    }
+    handleClick = () => {
+        this.setState({ working: !this.state.working });
+    };
+
+
+  render() {
+        const work = this.state.working ? 'work' : 'pause';
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className={work} alt="logo" />
+            <h1 className="App-title">Simpsons Quotes</h1>
+              <button className={"btn btn-primary"} onClick={this.handleClick}>
+                  {work.toUpperCase()}
+              </button>
+          </header>
+
+            <Lamp on/>
+            <Lamp />
+            <Quotes/>
+        </div>
+    );
+  }
 }
 
 export default App;
